d47abca4d390dfdd7b634523f95f679e082eaf60
estradakev <kevinestrada530@gmail.com>
2023-04-30 01:46:46 -0700

    Created a new components

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,28 @@ import React, {useState} from 'react';
 import './App.css';
 import InputField from './components/InputField';
 import { Todo } from './model';
+import TodoList from './components/TodoList';
+
 
 const App: React.FC = () => {
 
   const [todo, setTodo] = useState<string>("");
   const [todos, setTodos] = useState<Todo[]>([]); // how you create an array from an interface
 
-  const handleAdd = () => {
-    
-  };
+  const handleAdd = (event: React.FormEvent) => {
+    event.preventDefault();
 
+    if(todo){
+      setTodos([...todos, {id: Date.now(), todo, isDone:false}]);
+      setTodo("");
+    }
+  };
 
   return ( 
     <div className='App'>
       <span className="heading">Taskify</span>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/> 
+      <TodoList todos={todos} setTodos={setTodos}/>
     </div>
   )
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,21 +1,28 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import "./styles.css"
 
 interface Props {
 	todo: string;
 	setTodo: React.Dispatch<React.SetStateAction<string>>;
-	handleAdd: () => void;
+	handleAdd: (e: React.FormEvent) => void;
 }
  
-const InputField = ({todo, setTodo} : Props) => {
+const InputField = ({todo, setTodo, handleAdd} : Props) => {
+	const inputRef = useRef<HTMLInputElement>(null)
+	
   return (
-	<form className='input'>
-		<input type="input" 
-		value={todo}
-		onChange={
-			(e) => setTodo(e.target.value)
-		}
-		placeholder='Enter Task' className='input__box' />
+	<form className='input' onSubmit={(e)=> {
+			handleAdd(e)
+			inputRef.current?.blur();
+		}}>
+		<input 
+			ref={inputRef}
+			type="input" 
+			value={todo}
+			onChange={
+				(e) => setTodo(e.target.value)
+			}
+			placeholder='Enter Task' className='input__box' />
 		<button className='input__submit' type='submit'>
 			Go 
 		</button>
