2fb6c37abd42ce5a26ffd1ce26dd9296bb05bee4
estradakev <kevinestrada530@gmail.com>
2023-04-29 18:46:53 -0700

    Began to create the input form

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import InputField from './components/InputField';
+import { Todo } from './model';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App: React.FC = () => {
+
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]); // how you create an array from an interface
+
+  const handleAdd = () => {
+    
+  };
+
+
+  return ( 
+    <div className='App'>
+      <span className="heading">Taskify</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/> 
     </div>
-  );
+  )
 }
 
 export default App;
