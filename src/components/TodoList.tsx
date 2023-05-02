481d8484ed67eca2f89f3b14863455847d5a6ff3
estradakev <kevinestrada530@gmail.com>
2023-05-01 20:33:40 -0700

    Added functionality to the inputs and styled it as well

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Todo } from "../model"
 import  "./styles.css"
+import SingleTodo from "./SingleTodo";
 
 interface Props{
 	todos: Todo[];
@@ -12,7 +13,12 @@ const TodoList = ({todos, setTodos} : Props) => {
 		<div className="todos">
 			{
 				todos.map(todo =>(
-					<li> {todo.todo}</li>
+					<SingleTodo 
+					todo={todo} 
+					key={todo.id}
+					todos = {todos}
+					setTodos={setTodos}
+					/>
 				))
 			}
 		</div>
